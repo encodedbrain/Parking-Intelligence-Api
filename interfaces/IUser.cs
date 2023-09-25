441755b735aa3c87b0222d05cbeaf4ee69a5a6b6
marcoDmc <marcodamasceno07@gmail.com>
2023-09-25 12:55:22 -0300

    feat: (IData / IUser) added intefaces

---
+++ interfaces/IUser.cs
@@ -0,0 +1,13 @@
+namespace Parking_Intelligence_Api.interfaces
+{
+    public interface IUser
+    {
+        public bool VaLidateEmail(string? email);
+        public string EncryptingPassword(string? password);
+        public bool ValidateName(string? name);
+        public bool ValidateCpf(string? cpf);
+        public string ReturnCpfFormated(string? cpf);
+        public bool ValidatePhone(string? phone);
+        public bool ValidatePassword(string password);
+    }
+}
