5af21896a4d8d0566286fd5ea8b63fa9e1f72e54
marcoDmc <marcodamasceno07@gmail.com>
2023-09-27 03:36:23 -0300

    style: (Program.cs) remove whitespace

--- Program.cs
+++ Program.cs
@@ -15,7 +15,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddControllers();
 builder.Services.AddDbContext<ParkingDB>();
-
 builder.Services.AddCors(options =>
 {
     options.AddPolicy(
