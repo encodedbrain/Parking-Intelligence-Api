ead57836321da3853cd9417274470a453bb6a9f0
marcoDmc <marcodamasceno07@gmail.com>
2023-10-19 10:08:59 -0300

    build: remove xUnit

--- Services/User/LoginServices.cs
+++ Services/User/LoginServices.cs
@@ -1,40 +1,39 @@
 using Parking_Intelligence_Api.Data;
 using Parking_Intelligence_Api.Schemas;
 
-namespace Parking_Intelligence_Api.Services.User
+namespace Parking_Intelligence_Api.Services.User;
+
+public class LoginServices
 {
-    public class LoginServices
+    private Models.User _user = new();
+
+    internal object ReturnUser(LoginSchema prop
+    )
     {
-        private Models.User _user = new Models.User();
-        internal object? ReturnUser(LoginSchema prop
-        )
+        using (var db = new ParkingDb())
         {
-            using (var db = new ParkingDb())
-            {
-                var gettingUser = db.Users.SingleOrDefault(
-                    user => user.Email == prop.Email && user.Password == _user.EncryptingPassword(prop.Password)
-                );
+            var user = db.Users.SingleOrDefault(
+                user => user.Email == prop.Email && user.Password == _user.EncryptingPassword(prop.Password)
+            );
 
-                if (gettingUser is null)
-                    return null;
-
-
-                var token = ParkingServices.GenerateToken(gettingUser);
-                gettingUser.Password = string.Empty;
-
-                return new { gettingUser, token };
-            }
-                
-        }
-
-        internal bool ValidateCredentials(LoginSchema prop)
-        {
-            if (string.IsNullOrEmpty(prop.Email) || string.IsNullOrEmpty(prop.Password))
-                return false;
-            if (!new Models.User().ValidatePassword(prop.Password) || !new Models.User().VaLidateEmail(prop.Email))
+            if (user is null)
                 return false;
 
-            return true;
+
+            var token = ParkingServices.GenerateToken(user);
+            user.Password = string.Empty;
+
+            return new { user, token };
         }
+    }
+
+    internal bool ValidateCredentials(LoginSchema prop)
+    {
+        if (string.IsNullOrEmpty(prop.Email) || string.IsNullOrEmpty(prop.Password))
+            return false;
+        if (!new Models.User().ValidatePassword(prop.Password) || !new Models.User().VaLidateEmail(prop.Email))
+            return false;
+
+        return true;
     }
 }
