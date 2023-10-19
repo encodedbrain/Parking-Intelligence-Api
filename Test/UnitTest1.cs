0bac487567f045ab6e5c24130374b39defb4a52e
marcoDmc <marcodamasceno07@gmail.com>
2023-10-19 10:36:44 -0300

    test: adding base code for endpoint testing

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -3,8 +3,13 @@ namespace Test;
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public async Task Test1()
     {
+        // await using var application = new WebApplicationFactory<Program>();
+        // using var client = application.CreateClient();
 
+        // var response = await client.GetStringAsync("v1");
+
+        // Assert.Equal("Hello World!", response);
     }
 }
