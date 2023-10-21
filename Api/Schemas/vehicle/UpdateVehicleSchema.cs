bae1f40ed522dda285873ba366dc141ba8c77f5f
marcoDmc <marcodamasceno07@gmail.com>
2023-10-21 03:25:47 -0300

    chore: added schema

--- Api/Schemas/vehicle/UpdateVehicleSchema.cs
+++ Api/Schemas/vehicle/UpdateVehicleSchema.cs
@@ -9,14 +9,16 @@ public class UpdateVehicleSchema
         
     }
 
-    public UpdateVehicleSchema(string email, string password, string vehicleIdentifier)
+    public UpdateVehicleSchema(string email, string password, string vehicleIdentifier , string method)
     {
         Email = email;
         Password = password;
         VehicleIdentifier = vehicleIdentifier;
+        Method = method;
     }
 
     public string Email { get; set; } = null!;
     public string Password { get; set; } = null!;
     public string VehicleIdentifier { get; set; } = null!;
+    public string Method { get; set; } = null!;
 }
