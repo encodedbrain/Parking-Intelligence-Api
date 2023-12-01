0d590dd0f478140fed278350b3ccef64bb0ff513
marcoDmc <marcodamasceno07@gmail.com>
2023-12-01 18:35:12 -0300

    chore: create new schema

---
+++ Schemas/User/UpdatePasswordSchema.cs
@@ -0,0 +1,15 @@
+﻿namespace Parking_Intelligence_Api.Schemas.User;
+
+public class UpdatePasswordSchema
+{
+    public UpdatePasswordSchema(string email, string password, string newPassword)
+    {
+        Email = email;
+        Password = password;
+        NewPassword = newPassword;
+    }
+
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string NewPassword { get; set; }
+}
